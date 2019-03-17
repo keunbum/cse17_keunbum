75654979fc92743f3047dcc14729fc5eac06466f
keunbum <rkeunbum@gmail.com>
2019-03-18 01:10:06 +0900

    2019.03.18-1th

---
+++ Baekjoon/notsolved/2502/sol1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>   
+#include <ctime>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int d, k;
+  cin >> d >> k;
+  int v[30] = {1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040};
+  int a, b = 1;
+  for (a = 1; a < k / 2 + 1; a++) {
+    if ((k - a * v[d - 3]) % v[d - 2] == 0) {
+      b = (k - a * v[d - 3]) / v[d - 2];
+      break;
+    }    
+  }  
+  cout << a << '\n' << b << '\n';
+//cerr << (float)clock()/CLOCKS_PER_SEC << " seconds." << '\n';
+  return 0;
+}
