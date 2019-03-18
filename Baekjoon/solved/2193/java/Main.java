b14be3f6177e336c7b85785d1a18a7700da75ab9
keunbum <rkeunbum@gmail.com>
2019-03-19 00:51:36 +0900

    2019.03.19.1th

---
+++ Baekjoon/solved/2193/java/Main.java
@@ -0,0 +1,57 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+  public static void main(String[] args) {
+    InputStream inputStream = System.in;
+    OutputStream outputStream = System.out;
+    InputReader in = new InputReader(inputStream);
+    PrintWriter out = new PrintWriter(outputStream);
+    TaskA solver = new TaskA();
+    solver.solve(1, in, out);
+    out.close();
+  }
+
+  static class TaskA {
+    public void solve(int testNumber, InputReader in, PrintWriter out) {
+      BigInteger[] dp = new BigInteger[91];
+      dp[0] = BigInteger.valueOf(0);
+      dp[1] = BigInteger.valueOf(1);
+      for (int i = 2; i < 91; i++)
+        dp[i] = dp[i - 2].add(dp[i - 1]);
+      int n = in.nextInt();
+      out.println(dp[n]);
+    }
+  }
+
+  static class InputReader {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+      reader = new BufferedReader(new InputStreamReader(stream), 32768);
+      tokenizer = null;
+    }
+
+    public String next() {
+      while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        try {
+          tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
+      }
+      return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+  }
+}
