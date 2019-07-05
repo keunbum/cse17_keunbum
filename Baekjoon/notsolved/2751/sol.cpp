7c8fdae900868dfd79a0b16fdd9f918835523f64
keunbum <rkeunbum@gmail.com>
2019-07-05 18:15:50 +0900

    2019.07.05-first

--- Baekjoon/notsolved/2751/sol.cpp
+++ Baekjoon/notsolved/2751/sol.cpp
@@ -15,20 +15,9 @@ int main() {
   for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
-  make_heap(a, a + n);
-  sort_heap(a, a + n);
+  quick_sort(a, a + n);
   for (int i = 0; i < n; i++) {
     cout << a[i] << ' ';
-  }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      int a, b;
-      cin >> a >> b;
-      int c = a & b;
-      int d = a | b;
-      int g = c % d;
-      cout << c << ' ' << d << ' ' << g << '\n';
-    }
   }
   return 0;
 }
--- Baekjoon/notsolved/2751/sol3.cpp
+++ Baekjoon/notsolved/2751/sol3.cpp
@@ -1,11 +1,45 @@
+// Mergesort is a stable sort.
+
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 const int N = (int) 1e6;
 
 int a[N];
+int buf[N];
+
+// [first, middle) ~ [middle, last)
+template <typename RandomAccessIterator>
+void merge(RandomAccessIterator first, RandomAccessIterator middle, RandomAccessIterator last) {
+  int la = (int) (middle - first);
+  int lb = (int) (last - middle);
+  int i = 0;
+  int j = 0;
+  int k = 0;
+  while (i < la) {
+    *(buf + j++) = *(first + i++);
+  }
+  i = 0;
+  j = 0;
+  while (i < lb && j < la) {
+    *(first + k++) = *(buf + j) < *(middle + i) ? *(buf + j++) : *(middle + i++);
+  }
+  while (j < la) {
+    *(first + k++) = *(buf + j++);
+  }
+}
+
+// [first, last)
+template <typename RandomAccessIterator>
+void merge_sort(RandomAccessIterator first, RandomAccessIterator last) {
+  int n = (int) (last - first);
+  if (n <= 1) return;
+  RandomAccessIterator middle = first + n / 2;
+  merge_sort(first, middle);
+  merge_sort(middle, last);
+  merge(first, middle, last);
+}
 
 int main() {
   ios::sync_with_stdio(false);
@@ -15,7 +49,7 @@ int main() {
   for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
-  stable_sort(a, a + n);
+  merge_sort(a, a + n);
   for (int i = 0; i < n; i++) {
     cout << a[i] << ' ';
   }
