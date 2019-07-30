fe38aed6febf1c088ce12f1e51c5b74d2c0c2f3b
keunbum <rkeunbum@gmail.com>
2019-07-31 00:27:05 +0900

    2019.07.31

--- Baekjoon/solved/15953/sol.cpp
+++ Baekjoon/solved/15953/sol.cpp
@@ -2,30 +2,35 @@
 
 using namespace std;
 
-int f7[] = {0, 500, 300, 200, 50, 30, 10};
-int f17[123], f18[123];
+int x[] = { 500, 300, 200, 50, 30, 10 }; 
+int y[] = { 512, 256, 128, 64, 32 };
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  int cnt = 1;
-  for (int i = 1; i <= 6; i++) {
-    for (int j = 0; j < i; j++) {
-      f17[cnt++] = f7[i];
-    }
-  }
-  cnt = 1;
-  for (int i = 1; i <= 5; i++) {
-    for (int j = 0; j < 1 << (i - 1); j++) {
-      f18[cnt++] = 512 >> (i - 1);
-    }
-  }
   int tt;
   cin >> tt;
   while (tt--) {
     int a, b;
     cin >> a >> b;
-    cout << (f17[a] + f18[b]) * 10000 << '\n';
+    if (a == 0) a = 101;
+    if (b == 0) b = 65;
+    int ans = 0;
+    for (int i = 0; i <= 5; i++) {
+      a -= i + 1;
+      if (a <= 0) {
+        ans += x[i];
+        break;
+      }
+    }
+    for (int i = 0; i < 5; i++) {
+      b -= (1 << i);
+      if (b <= 0) {
+        ans += y[i];
+        break;
+      }
+    }
+    cout << 10000 * ans << '\n';
   }
   return 0;
 }
