d1bc17eb10380dd486f48ec58cfaa1a5e66619eb
keunbum <rkeunbum@gmail.com>
2019-07-31 10:52:19 +0900

    2019.07.31

---
+++ Baekjoon/notsolved/3665/sol.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+      cin >> a[i];
+    }
+    int m;
+    cin >> m;
+
+  }
+  return 0;
+}
