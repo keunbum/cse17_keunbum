66a9a76aa0fb80ab6613bb9a16b9455c66f5cd19
keunbum <rkeunbum@gmail.com>
2019-08-12 00:17:25 +0900

    2019.08.12

--- Baekjoon/notsolved/11437/sol.cpp
+++ Baekjoon/notsolved/11437/sol.cpp
@@ -2,160 +2,42 @@
 
 using namespace std;
 
-template <typename T>
-class graph {
-  public:
-  struct edge {
-    int from;
-    int to;
-    T cost;
-  };
-  vector<edge> edges;
-  vector<vector<int>> g;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
   int n;
-  function<bool(int)> ignore;
-  graph(int _n) : n(_n) {
-    g.resize(n);
-    ignore = nullptr;
+  cin >> n;
+  vector<vector<int>> g(n);
+  for (int i = 0; i < n - 1; i++) {
+    int x, y;
+    cin >> x >> y;
+    x--; y--;
+    // 0-indexed vertices
+    g[x].push_back(y);
+    g[y].push_back(x);
+    // undirected graph
   }
-  virtual int add(int from, int to, T cost) = 0;
-  virtual void set_ignore_edge_rule(const function<bool(int)> &f) {
-    ignore = f;
-  }
-  virtual void reset_ignore_edge_rule() {
-    ignore = nullptr;
-  }
-};
-
-template <typename T>
-class forest : public graph<T> {
-  public:
-  using graph<T>::edges;
-  using graph<T>::g;
-  using graph<T>::n;
-  forest(int _n) : graph<T>(_n) {
-  }
-  int add(int from, int to, T cost = 1) {
-    assert(0 <= from && from < n && 0 <= to && to < n);
-    int id = (int) edges.size();
-    assert(id < n - 1);
-    g[from].push_back(id);
-    g[to].push_back(id);
-    edges.push_back({from, to, cost});
-    return id;
-  }
-};
-
-template <typename T>
-class dfs_forest : public forest<T> {
-  public:
-  using forest<T>::edges;
-  using forest<T>::g;
-  using forest<T>::n;
-  using forest<T>::ignore;
-  vector<int> pv;
-  vector<int> pe;
-  vector<int> order;
-  vector<int> pos;
-  vector<int> end;
-  vector<int> sz;
-  vector<int> root;
-  vector<int> depth;
-  vector<T> dist;
-  dfs_forest(int _n) : forest<T>(_n) {
-  }
-  void init() {
-    pv = vector<int>(n, -1);
-    pe = vector<int>(n, -1);
-    order.clear();
-    pos = vector<int>(n, -1);
-    end = vector<int>(n, -1);
-    sz = vector<int>(n, 0);
-    root = vector<int>(n, -1);
-    depth = vector<int>(n, -1);
-    dist = vector<T>(n);
-  }
-  void clear() {
-    pv.clear();
-    pe.clear();
-    order.clear();
-    pos.clear();
-    end.clear();
-    sz.clear();
-    root.clear();
-    depth.clear();
-    dist.clear();
-  }
-  private:
-  void do_dfs(int v) {
-    pos[v] = (int) order.size();
+  vector<int> pv(n, -1), order, pos(n, -1), depth(n, -1), en(n, -1);
+  depth[0] = 0; // 0-indexed depth
+  function<void(int)> Dfs = [&](int v) {
+    pos[v] = (int) order.size(); // search order
     order.push_back(v);
-    sz[v] = 1;
-    for (int id : g[v]) {
-      if (id == pe[v] || (ignore != nullptr && ignore(id))) {
+    for (int u : g[v]) {
+      if (depth[u] >= 0) {
         continue;
       }
-      auto &e = edges[id];
-      int to = e.from ^ e.to ^ v;
-      depth[to] = depth[v] + 1;
-      dist[to] = dist[v] + e.cost;
-      pv[to] = v;
-      pe[to] = id;
-      root[to] = (root[v] != -1 ? root[v] : to);
-      do_dfs(to);
-      sz[v] += sz[to];
+      depth[u] = depth[v] + 1;
+      pv[u] = v; // parent
+      Dfs(u);
     }
-    end[v] = (int) order.size() - 1;
-  }
-  void do_dfs_from(int v) {
-    depth[v] = 0;
-    dist[v] = T{};
-    root[v] = v;
-    pv[v] = pe[v] = -1;
-    do_dfs(v);
-  }
-  public:
-  void dfs(int v, bool clear_order = true) {
-    if (pv.empty()) {
-      init();
-    } else {
-      if (clear_order) {
-        order.clear();
-      }
-    }
-    do_dfs_from(v);
-  }
-  void dfs_all() {
-    init();
-    for (int v = 0; v < n; v++) {
-      if (depth[v] == -1) {
-        do_dfs_from(v);
-      }
-    }
-    assert((int) order.size() == n);
-  }
-};
-
-template <typename T>
-class lca_forest : public dfs_forest<T> {
-  public:
-  using dfs_forest<T>::edges;
-  using dfs_forest<T>::g;
-  using dfs_forest<T>::n;
-  using dfs_forest<T>::pv;
-  using dfs_forest<T>::pos;
-  using dfs_forest<T>::end;
-  using dfs_forest<T>::depth;
+    en[v] = (int) order.size() - 1;
+    // last vertex's index in order in v's subtree
+  };
+  Dfs(0);
+  vector<vector<int>> pr;
   int h;
-  vector< vector<int> > pr;
-  lca_forest(int _n) : dfs_forest<T>(_n) {
-  }
-  inline void build_lca() {
-    assert(!pv.empty());
-    int max_depth = 0;
-    for (int i = 0; i < n; i++) {
-      max_depth = max(max_depth, depth[i]);
-    }
+  auto Build = [&]() {
+    int max_depth = *max_element(depth.begin(), depth.end());
     h = 1;
     while ((1 << h) <= max_depth) {
       h++;
@@ -164,57 +46,53 @@ class lca_forest : public dfs_forest<T> {
     for (int i = 0; i < n; i++) {
       pr[i].resize(h);
       pr[i][0] = pv[i];
+      // the ancestor just above(2^0 = 1th) is the parent
     }
     for (int j = 1; j < h; j++) {
       for (int i = 0; i < n; i++) {
-        pr[i][j] = (pr[i][j - 1] == -1 ? -1 : pr[pr[i][j - 1]][j - 1]);
+        pr[i][j] = pr[i][j - 1] == -1 ? -1 : pr[pr[i][j - 1]][j - 1];
       }
     }
-  }
-  inline bool anc(int x, int y) {
-    return (pos[x] <= pos[y] && end[y] <= end[x]);
-  }
-  inline int lca(int x, int y) {
-    // maybe optimize?
-    // if depth[x] > depth[y], swap
-    // then go from j = log(depth[x])?
-    assert(!pr.empty());
-    if (anc(x, y)) {
+    
+    for (int i = 0; i < n; i++) {
+      cerr << i << "'s 2^k-th anc:";
+      for (int j = 0; j < h; j++) {
+        cerr << ' ' << pr[i][j];
+      }
+      cerr << '\n';
+    }
+    
+  };
+  Build();
+  auto Anc = [&](int x, int y) {
+    return pos[x] <= pos[y] && en[y] <= en[x];
+    // x is y's ancestor
+  };
+  auto Lca = [&](int x, int y) {
+    if (Anc(x, y)) {
       return x;
     }
-    if (anc(y, x)) {
+    if (Anc(y, x)) {
       return y;
     }
+    if (depth[x] > depth[y]) {
+      swap(x, y);
+    }
     for (int j = h - 1; j >= 0; j--) {
-      if (pr[x][j] != -1 && !anc(pr[x][j], y)) {
+      if (pr[x][j] != -1 && !Anc(pr[x][j], y)) {
         x = pr[x][j];
       }
     }
     return pr[x][0];
-  }
-};
-
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int n;
-  cin >> n;
-  lca_forest<int> f(n);
-  for (int i = 0; i < n - 1; i++) {
-    int x, y;
-    cin >> x >> y;
-    x--; y--;
-    f.add(x, y, 1);
-  }
-  f.dfs(0);
-  f.build_lca();
+  };
   int m;
   cin >> m;
   while (m--) {
     int x, y;
     cin >> x >> y;
     x--; y--;
-    cout << f.lca(x, y) + 1 << '\n';
+    cout << Lca(x, y) + 1 << '\n';
+    // change from 0-indexed to 1-indexed vertex
   }
   return 0;
 }
--- Baekjoon/notsolved/11437/sol2.cpp
+++ Baekjoon/notsolved/11437/sol2.cpp
@@ -1,94 +1,67 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int n, h;
-vector<vector<int>> g, pr;
-vector<int> pv, order, pos, sz, depth, root, en;
-
-void dfs(int v) {
-  pos[v] = (int) order.size();
-  order.push_back(v);
-  sz[v] = 1;
-  for (int u : g[v]) {
-    if (depth[u] >= 0) {
-      continue;
-    }
-    depth[u] = depth[v] + 1;
-    pv[u] = v;
-    root[u] = root[v];
-    dfs(u);
-    sz[v] += sz[u];
-  }
-  en[v] = (int) order.size() - 1;
-}
-
-void build() {
-  int max_depth = *max_element(depth.begin(), depth.end());
-  h = 1;
-  while ((1 << h) <= max_depth) {
-    h++;
-  }
-  pr.resize(n);
-  for (int i = 0; i < n; i++) {
-    pr[i].resize(h);
-    pr[i][0] = pv[i];
-  }
-  for (int j = 1; j < h; j++) {
-    for (int i = 0; i < n; i++) {
-      pr[i][j] = pr[i][j - 1] == -1 ? -1 : pr[pr[i][j - 1]][j - 1];
-    }
-  }
-}
-
-inline bool anc(int x, int y) {
-  return pos[x] <= pos[y] && en[y] <= en[x];
-}
-
-inline int lca(int x, int y) {
-  if (anc(x, y)) return x;
-  if (anc(y, x)) return y;
-  if (depth[x] > depth[y]) swap(x, y);
-  for (int j = log2(depth[x]); j >= 0; j--) {
-    if (pr[x][j] != -1 && !anc(pr[x][j], y)) {
-      x = pr[x][j];
-    }
-  }
-  return pr[x][0];
-}
-
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  cin >> n;
-  g.resize(n);
-  pv.resize(n, -1);
-  pos.resize(n, -1);
-  sz.resize(n, 0);
-  depth.resize(n, -1);
-  root.resize(n, -1);
-  en.resize(n, -1);
-  for (int i = 0; i < n - 1; i++) {
-    int x, y;
-    cin >> x >> y;
-    x--; y--;
-    g[x].push_back(y);
-    g[y].push_back(x);
-  }
-  depth[0] = 0;
-  root[0] = 0;
-  dfs(0);
-  build();
-  int m;
-  cin >> m;
-  while (m--) {
-    int x, y;
-    cin >> x >> y;
-    x--; y--;
-    cout << lca(x, y) + 1 << '\n';
-  }
-  return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<vector<int>> g(n + 1);
+	for (int i = 0; i < n - 1; i++) {
+		int x, y;
+		cin >> x >> y;
+		g[x].push_back(y);
+		g[y].push_back(x);
+	}
+	vector<vector<int>> r(2 * n + 1, vector<int>(20));
+	for (int i = 0; i < 20; i++) {
+		r[1][i] = 1;
+	}
+	vector<int> depth(2 * n + 1);
+	function<void(int)> Dfs = [&](int v){
+		for (int u : g[v]) {
+			if (u != r[v][0]) {
+				r[u][0] = v;
+				depth[u] = depth[v] + 1;
+				for (int i = 1; i < 20; i++) {
+					r[u][i] = r[r[u][i - 1]][i - 1];
+				}
+				Dfs(u);
+			}
+		}
+	};
+	Dfs(1);
+	auto Anc = [&](int x, int d) {
+		for (int i = 0; i < 20; i++) {
+			if (d & (1 << i)) {
+				x = r[x][i];
+			}
+		}
+		return x;
+	};
+	auto GetLca = [&](int x, int y) {
+		if (depth[x] > depth[y]) {
+			swap(x, y);
+		}
+		y = Anc(y, depth[y] - depth[x]);
+		if (x == y) {
+			return x;
+		}
+		for (int i = 19; i >= 0; i--) {
+			if (r[x][i] != r[y][i]) {
+				x = r[x][i];
+				y = r[y][i];
+			}
+		}
+		return r[x][0];
+	};
+	int m;
+	cin >> m;
+	while (m--) {
+		int x, y;
+		cin >> x >> y;
+		cout << GetLca(x, y) << '\n';
+	}
+	return 0;
 }
