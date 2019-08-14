eb9dd625b73a2e9493a746c7272b64c18ee5e962
keunbum <rkeunbum@gmail.com>
2019-08-14 23:14:04 +0900

    2019.08.14

---
+++ Baekjoon/solved/2150/sol2.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> g(n), g_rev(n);
+	while (m--) {
+		int x, y;
+		cin >> x >> y;
+		x--; y--;
+		g[x].push_back(y);
+		g_rev[y].push_back(x);
+	}
+	vector<int> order;
+	vector<bool> was(n, false);
+	function<void(int)> Dfs1 = [&](int v) {
+		for (int u : g[v]) {
+			if (!was[u]) {
+				was[u] = true;
+				Dfs1(u);
+			}
+		}
+		order.push_back(v);
+	};
+	for (int i = 0; i < n; i++) {
+		if (!was[i]) {
+			was[i] = true;
+			Dfs1(i);
+		}
+	}
+	fill(was.begin(), was.end(), false);
+	vector<vector<int>> ans;
+	function<void(int)> Dfs2 = [&](int v) {
+		for (int u : g_rev[v]) {
+			if (!was[u]) {
+				was[u] = true;
+				ans.back().push_back(u);
+				Dfs2(u);
+			}
+		}
+	};
+	for (int i = n - 1; i >= 0; i--) {
+		int v = order[i];
+		if (!was[v]) {
+			was[v] = true;
+			ans.push_back(vector<int>(1, v));
+			Dfs2(v);
+		}
+	}
+	for (auto& s : ans) {
+		sort(s.begin(), s.end());
+	}
+	sort(ans.begin(), ans.end());
+	cout << ans.size() << '\n';
+	for (auto& s : ans) {
+		for (int x : s) {
+			cout << x + 1 << ' ';
+		}
+		cout << -1 << '\n';
+	}
+	return 0;
+}
